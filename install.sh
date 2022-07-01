60466c6a3b7f3f00418ff84b518b13effc41b1f9
Juan Gonzalez <jrg2156@gmail.com>
2022-07-01 02:42:59 +0000

    update

--- install.sh
+++ install.sh
@@ -5,8 +5,21 @@ set -eou pipefail
 # Get the directory in which this script lives.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Install Fish shell
-echo "Installing fish..."
-$SCRIPT_DIR/install_fish.sh
+sudo apt-get update
+sudo apt-get install -y \
+    fish \
+    tldr \
+    ripgrep \
+    fzf \
+    hub
 
-# Clone config files
+# Copy personal scripts to PATH
+cp $SCRIPT_DIR/scripts/* $HOME/.local/bin
+
+# Make sure ~/.local/bin is in the PATH
+if ! [ -x git_is_repo ]; then
+
+fi
+
+# Set fish prompt
+cp $SCRIPT_DIR/.config/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish

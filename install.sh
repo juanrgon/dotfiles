b94fb2bdff3666e6cfbbba11a0e499b542a77c7e
Juan Gonzalez <jrg2156@gmail.com>
2022-07-01 18:58:05 +0000

    fix path setting

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 set -eou pipefail
 
+###############################################
 # Get the directory in which this script lives.
+###############################################
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 sudo apt-get update
@@ -13,10 +15,30 @@ sudo apt-get install -y \
     fzf \
     hub
 
-# Copy personal scripts to PATH
-chmod +x scripts/*
-cp $SCRIPT_DIR/scripts/* $HOME/.local/bin
+#######################
+# Setup fish config dir
+#######################
+FISH_CONF_DIR=$HOME/.config/fish
+mkdir -p $FISH_CONF_DIR
 
-# Set fish prompt
-mkdir -p $HOME/.config/fish/functions/
-cp $SCRIPT_DIR/.config/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
+###############################
+# Copy personal scripts to PATH
+###############################
+PERSONAL_SCRIPTS_DIR="$HOME/.juan/scripts"      # destination for my personal scripts
+mkdir -p "$PERSONAL_SCRIPTS_DIR/.juan/scrips/"  # Create personal scripts dir
+chmod +x scripts/*                              # Make personal scripts files executable
+cp $SCRIPT_DIR/scripts/* $PERSONAL_SCRIPTS_DIR  # Copy scripts to my personal scripts dir
+# Add export statment to fish config file
+(cat >> $FISH_CONF_DIR/config.fish) <<PATH_EXPORT_STMT
+
+##############################
+# Add personal scripts to PATH
+##############################
+export \$PATH=$PERSONAL_SCRIPTS_DIR:\$PATH
+PATH_EXPORT_STMT
+
+##################
+# Set shell prompt
+##################
+mkdir -p $FISH_CONF_DIR/functions/
+cp $SCRIPT_DIR/.config/fish/functions/fish_prompt.fish $FISH_CONF_DIR/functions/fish_prompt.fish

3d7e055f235aee2e1616aadb4195f36aa76a837b
Juan Gonzalez <jrg2156@gmail.com>
2022-07-01 03:13:46 +0000

    fix prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -104,3 +104,59 @@ function streamline_prompt
     end
     echo -n -s (set_color normal) (set_color $bg_color) $divider ' '
 end
+
+############
+# STREAMLINE
+############
+function streamline_os_icon_segment
+    set -l os_icon
+    switch (uname)
+        case Darwin
+	    set os_icon ''
+        case Linux
+            set os_icon ''
+        case Windows_NT
+	    set os_icon ''
+    end
+    echo $os_icon
+    echo black
+    echo white
+end
+
+function streamline_dir_segment
+    echo (prompt_pwd)  # prompt_pwd is a built-in fish function
+    echo blue
+    echo black
+end
+
+function streamline_git_segment
+    if git_is_repo  # git_is_repo is a built-in fish function
+        echo " "(git_branch_name)  # git_branch_name is a built-in fish function
+        if git_is_dirty # git_branch_name is a built-in fish function
+            echo red
+        else if git_is_staged  # git_is_staged is a built-in fish function
+            echo yellow
+        else
+            echo green
+        end
+        echo black
+    end
+end
+
+# what to show at the beginning of the first line of the prompt
+function streamline_first_line_leader
+    echo '╭─'
+    echo normal
+    echo blue
+end
+
+# what to show at the beginning of the second line of the prompt
+function streamline_second_line_leader
+    echo '╰─'
+    echo normal
+    echo blue
+end
+
+set -x streamline_segments streamline_os_icon_segment streamline_dir_segment streamline_git_segment \n
+# define leaders to display on each line of the prompt
+set -x streamline_leaders streamline_first_line_leader streamline_second_line_leader

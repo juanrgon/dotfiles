2c8a83dc027e6fceceba2514587be9310e237e34
Juan Gonzalez <jrg2156@gmail.com>
2022-07-22 19:06:59 +0000

    fix

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+# juanrgon fish config
+
+############
+# Cheatsheet
+############
+# "set --universal" sets a variable in ALL fish shells, current and future
+# "set --export" sets a variable in the current shell, and in its future child processes
+
+#####################################
+# Add my personal scripts to the PATH
+#####################################
+set --universal PERSONAL_SCRIPTS_DIR $HOME/.juan/bin                 # destination for my personal scripts
+mkdir -p $PERSONAL_SCRIPTS_DIR                                       # Create personal scripts dir
+chmod +x $PERSONAL_SCRIPTS_DIR/*                                     # Make personal scripts files executable
+# NOTE using "set --universal" on fish_user_paths would cause it to get longer on each new shell
+set --export fish_user_paths $PERSONAL_SCRIPTS_DIR $fish_user_paths  # add personal scripts dir to the PATH
--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
+set -ou pipefail
 
 ###############################################
 # Get the directory in which this script lives.
 ###############################################
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+#####################
+# Install OS packages
+#####################
 sudo apt-get update
-sudo apt-get install -y \
+sudo apt-get install -y --no-install-recommends \
+    fd-find \
     fish \
     tldr \
     ripgrep \
@@ -20,25 +24,23 @@ sudo apt-get install -y \
 #######################
 FISH_CONF_DIR=$HOME/.config/fish
 mkdir -p $FISH_CONF_DIR
+touch $FISH_CONF_DIR/config.fish
 
-###############################
-# Copy personal scripts to PATH
-###############################
-PERSONAL_SCRIPTS_DIR="$HOME/.juan/scripts"      # destination for my personal scripts
-mkdir -p "$PERSONAL_SCRIPTS_DIR/.juan/scrips/"  # Create personal scripts dir
-chmod +x scripts/*                              # Make personal scripts files executable
-cp $SCRIPT_DIR/scripts/* $PERSONAL_SCRIPTS_DIR  # Copy scripts to my personal scripts dir
-# Add export statment to fish config file
-(cat >> $FISH_CONF_DIR/config.fish) <<PATH_EXPORT_STMT
+################################################
+# Copy personal scripts to $PERSONAL_SCRIPTS_DIR
+################################################
+PERSONAL_SCRIPTS_DIR="$HOME/.juan/bin"                   # destination for my personal scripts
+mkdir -p "$PERSONAL_SCRIPTS_DIR"                         # Create personal scripts dir
+cp $SCRIPT_DIR/scripts/* $PERSONAL_SCRIPTS_DIR           # Copy scripts to my personal scripts dir
+chmod +x $PERSONAL_SCRIPTS_DIR/*                         # Make personal scripts files executable
 
-##############################
-# Add personal scripts to PATH
-##############################
-set -gx PATH $PERSONAL_SCRIPTS_DIR \$PATH
-PATH_EXPORT_STMT
-
-##################
-# Set shell prompt
-##################
+##########################
+# Copy over fish functions
+##########################
 mkdir -p $FISH_CONF_DIR/functions/
 cp $SCRIPT_DIR/.config/fish/functions/fish_prompt.fish $FISH_CONF_DIR/functions/fish_prompt.fish
+
+#######################
+# Copy over fish config
+#######################
+cp $SCRIPT_DIR/.config/fish/config.fish $FISH_CONF_DIR/config.fish

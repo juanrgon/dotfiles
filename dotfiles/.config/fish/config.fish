6210fd35b3b944d1cd0ab0d5e0ada03deba373b8
Juan Gonzalez <jrg2156@gmail.com>
2022-09-26 11:45:11 -0400

    make fish config env var setting safer

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -17,15 +17,22 @@ mkdir -p $PERSONAL_SCRIPTS_DIR                          # Create personal script
 # NOTE using "set --universal" instead of "set --export" on fish_user_paths would cause it to get longer on each new shell
 set --export fish_user_paths $PERSONAL_SCRIPTS_DIR $fish_user_paths  # add personal scripts dir to the PATH
 
+##################################
+# Add homebrew bin dir to the PATH
+##################################
 switch (uname)
     case Darwin
-        set --export fish_user_paths /opt/homebrew/bin $fish_user_paths
+        if type --quiet brew
+            set --export fish_user_paths /opt/homebrew/bin $fish_user_paths
+        end
 end
 
 #################################
 # Add Cargo's bin dir to the PATH
 #################################
-set --export fish_user_paths $HOME/.cargo/bin $fish_user_paths  # add personal scripts dir to the PATH
+if type --quiet cargo
+    set --export fish_user_paths $HOME/.cargo/bin $fish_user_paths  # add personal scripts dir to the PATH
+end
 
 #######################################
 # Set code as EDITOR if it is installed

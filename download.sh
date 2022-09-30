560d9a94be080e8ca188a27566a40e79007c3105
Juan Gonzalez <juanrgon@github.com>
2022-09-30 14:00:25 -0400

    Auto-install git when downloading dotfiles

--- download.sh
+++ download.sh
@@ -2,12 +2,56 @@
 
 set -e
 
-DOTFILES_REPO_DESTINATION=$HOME/github/juanrgon/dotfiles
 
-# Clone dotfiles repo to ~/github/juanrgon/dotfiles
-git clone https://github.com/juanrgon/dotfiles.git $DOTFILES_REPO_DESTINATION
+main() {
+    # Install git if it's not installed
+    if ! command -v git > /dev/null; then
+        install_git
+    fi
 
-# Run dotfiles install script
-$DOTFILES_REPO_DESTINATION/install.sh
+    DOTFILES_REPO_DESTINATION=$HOME/github/juanrgon/dotfiles
 
+    # Clone dotfiles repo to ~/github/juanrgon/dotfiles
+    git clone https://github.com/juanrgon/dotfiles.git $DOTFILES_REPO_DESTINATION
 
+    # Run dotfiles install script
+    $DOTFILES_REPO_DESTINATION/install.sh
+
+    exec fish
+}
+
+install_git() {
+    echo Installing git
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install git
+    elif [[ -f /etc/os-release ]]; then
+        if (( $EUID == 0 )); then
+            SUDO=""
+        else
+            SUDO="sudo"
+        fi
+
+        DISTRO=$(cat /etc/os-release | grep ID_LIKE |  cut -d "=" -f 2 | tr -d '"' | cut -d " " -f 1)
+
+        case $DISTRO in
+            debian)
+                $SUDO apt update && $SUDO apt install -y git
+                ;;
+            centos|redhat)
+                $SUDO yum install -y git
+                ;;
+            arch)
+                $SUDO pacman -Sy --noconfirm git
+                ;;
+            *)
+                echo "Please install git manually, then try again"
+                exit 1
+        esac
+    else
+        echo "Please install git manually, then try again"
+        exit 1
+    fi
+}
+
+main

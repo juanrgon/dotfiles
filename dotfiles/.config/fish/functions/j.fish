6f50f4b506f2e56909248d587308031d0e69f504
Juan Gonzalez <jrg2156@gmail.com>
2023-07-05 14:59:31 -0400

    add j command to jump to github repo

---
+++ dotfiles/.config/fish/functions/j.fish
@@ -0,0 +1,16 @@
+function j
+    # Get the first argument
+    set -l arg (echo $argv[1])
+
+    set -l GITHUB_REPOS_DIR ~/github.com
+
+    # if there is a single repo that matches the argument, cd into it
+    # i.e. if $arg == motif and exactly one repo matches, cd into it
+    if test (fd --type d --max-depth 2 --min-depth 2 --base-directory $GITHUB_REPOS_DIR | grep -c $arg) -eq 1
+        cd $GITHUB_REPOS_DIR/(fd --type d --max-depth 2 --min-depth 2 --base-directory $GITHUB_REPOS_DIR | grep $arg)
+        return
+    end
+
+    # use $arg as the initial fzf query
+   cd ~/github.com/(fd --type d --max-depth 2 --min-depth 2 --base-directory ~/github.com | fzf --height 40% --reverse --prompt "cd into repo > " --query $arg)
+end
